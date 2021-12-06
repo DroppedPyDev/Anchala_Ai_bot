3a7c16ccfac73e31b7eadf5920935cb3b50e8cd1
Abhijith Sudhakaran <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2021-12-06 18:29:34 +0530

    Create audioplay.py

---
+++ handlers/audioplay.py
@@ -0,0 +1,74 @@
+
+from os import path
+import converter
+from callsmusic import callsmusic, queues
+from config import (
+    AUD_IMG,
+    BOT_USERNAME,
+    DURATION_LIMIT,
+    GROUP_SUPPORT,
+    QUE_IMG,
+    UPDATES_CHANNEL,
+)
+from handlers.play import convert_seconds
+from helpers.filters import command, other_filters
+from helpers.gets import get_file_name
+from pyrogram import Client
+from pytgcalls.types.input_stream import InputAudioStream
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+
+@Client.on_message(command(["stream", f"stream@{BOT_USERNAME}"]) & other_filters)
+async def stream(_, message: Message):
+    costumer = message.from_user.mention
+    lel = await message.reply_text("🔁 Pʀᴏᴄᴇssɪɴɢ Aᴜᴅɪᴏ ʏᴏᴜ Pʀᴏᴠɪᴅᴇᴅ...")
+
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text="✨ ɢʀᴏᴜᴘ", url=f"https://t.me/{GROUP_SUPPORT}"
+                ),
+                InlineKeyboardButton(
+                    text="🌻 ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{UPDATES_CHANNEL}"
+                ),
+            ]
+        ]
+    )
+
+    audio = message.reply_to_message.audio if message.reply_to_message else None
+    if not audio:
+        return await lel.edit("💭 **please reply to a telegram audio file**")
+    if round(audio.duration / 60) > DURATION_LIMIT:
+        return await lel.edit(
+            f"❌ **music with duration more than** `{DURATION_LIMIT}` **minutes, can't play !**"
+        )
+
+    title = audio.title
+    file_name = get_file_name(audio)
+    duration = convert_seconds(audio.duration)
+    file_path = await converter.convert(
+        (await message.reply_to_message.download(file_name))
+        if not path.isfile(path.join("downloads", file_name))
+        else file_name
+    )
+    ACTV_CALLS = {}
+    for x in callsmusic.pytgcalls.active_calls:
+        ACTV_CALLS.append(int(x.chat_id))    
+    if message.chat.id in ACTV_CALLS:
+        position = await queues.put(message.chat.id, file=file_path)
+        await message.reply_photo(
+            photo=f"{QUE_IMG}",
+            caption=f"💡 **Track added to queue »** `{position}`\n\n🏷 **Name:** {title[:50]}\n⏱ **Duration:** `{duration}`\n🎧 **Request by:** {costumer}",
+            reply_markup=keyboard,
+        )
+    else:
+        await callsmusic.pytgcalls.join_group_call(message.chat.id, InputAudioStream(file_path)) 
+        await message.reply_photo(
+            photo=f"{AUD_IMG}",
+            caption=f"🏷 **Name:** {title[:50]}\n⏱ **Duration:** `{duration}`\n💡 **Status:** `Playing`\n"
+            + f"🎧 **Request by:** {costumer}",
+            reply_markup=keyboard,
+        )
+
+    return await lel.delete() 
