95fe2aad8ceacb0a2d8c4f5c449260f425ff17db
Abhijith Sudhakaran <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2021-12-06 17:54:56 +0530

    Create config.py
    😂

---
+++ config.py
@@ -0,0 +1,48 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+from helpers.uptools import fetch_heroku_git_url
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+load_dotenv()
+que = {}
+admins = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME", "")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/fd294693240383d0a6af6.jpg")
+THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/4c39fbb88932761913fff.png")
+AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/73e10ed6e2bd32b478de6.png")
+QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/b95c13eef1ebd14dbb458.png")
+CMD_IMG = getenv("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME", "")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "")
+# Username without @
+OWNER_NAME = getenv("OWNER_NAME", "Telecat_X")
+# fill with your nickname
+ALIVE_NAME = getenv("ALIVE_NAME", "")
+# fill with your id as the owner of the bot
+OWNER_ID = int(os.environ.get("OWNER_ID",""))
+DATABASE_URL = os.environ.get("DATABASE_URL", "database_name")  # fill with your mongodb url
+# make a private channel and get the channel id
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL",""))
+# just fill with True or False (optional)
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+# UPDATER CONFIG
+U_BRANCH = "main"
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", "")
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "")
+UPSTREAM_REPO = os.environ.get(
+    "UPSTREAM_REPO", "https://github.com/Abhijith-Sudhakaran/MusicPlayer_TG"
+)
+HEROKU_URL = fetch_heroku_git_url(HEROKU_API_KEY, HEROKU_APP_NAME)
