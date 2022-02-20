622cac685b6c7f1d290d82b96b838bc9e296e975
Abhijith Sudhakaran <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2022-02-20 20:26:10 +0530

    Update botstart.py
    Some Unusual Things removed and Now its fully open source 🐱

--- handlers/botstart.py
+++ handlers/botstart.py
@@ -53,40 +53,36 @@ async def _human_time_duration(seconds):
 async def start_private(client: Client, message: Message):
     await message.reply_text(
         f"""✨ **Welcome {message.from_user.mention()} !**\n
-💭 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) allows you to play music on groups through the new Telegram's voice chats!**
-
-💡 **Find out all the Bot's commands and how they work by clicking on the » 📚 Commands button!**
-
-🥀 All Services Provided by [GBOTS](https://t.me/GEETHUBOTUPDATES) """,
+💭 **I am [{BOT_NAME}](https://t.me/{BOT_USERNAME})❤️ A RoBot for Music Playing and Downloading in Telegram!** """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "➕ Add me to Your Chat",
+                        "🎵 Add Me",
                         url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
                     )
                 ],
                 [InlineKeyboardButton("⚙️ Initial Setup", callback_data="cbhowtouse")],
                 [
-                    InlineKeyboardButton("📚 Commands", callback_data="cbcmds"),
+                    InlineKeyboardButton("📚 Functions", callback_data="cbcmds"),
                     InlineKeyboardButton("❤️ Creator", url=f"https://t.me/{OWNER_NAME}"),
                 ],
                 [
                     InlineKeyboardButton(
-                        "👥 Support Group", url=f"https://t.me/{GROUP_SUPPORT}"
+                        "👥 Support", url=f"https://t.me/{GROUP_SUPPORT}"
                     ),
                     InlineKeyboardButton(
-                        "📣 Update's Channel", url=f"https://t.me/{UPDATES_CHANNEL}"
+                        "📣 Update's", url=f"https://t.me/{UPDATES_CHANNEL}"
                     ),
                 ],
                 [
                     InlineKeyboardButton(
-                        "🌐 Source Code", url="https://t.me/GEETHUBOTUPDATES/20"
+                        "🌐 Source Code", url="https://github.com/Abhijith-Sudhakaran/MusicPlayer_TG"
                     )
                 ],
             ]
         ),
-        disable_web_page_preview=False,
+        disable_web_page_preview=True,
     )
 
 
@@ -129,7 +125,7 @@ async def help(client: Client, message: Message):
 
 ⚡ __Powered by {BOT_NAME} A.I__""",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="⚙️ Initial Setup", callback_data="cbguide")]]
+            [[InlineKeyboardButton(text="⚙️ Basics", callback_data="cbguide")]]
         ),
     )
 
@@ -152,5 +148,5 @@ async def get_uptime(client: Client, message: Message):
         "🤖 bot status:\n"
         f"• **uptime:** `{uptime}`\n"
         f"• **start time:** `{START_TIME_ISO}`"
-        f"• **Server:** www.heroku.com"
+        f"• **Server:** Heroku"
     )
